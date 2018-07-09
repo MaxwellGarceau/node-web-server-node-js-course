92a4cf284e52b85d79db07a991f501c5be3a0f7e
Max Garceau <maxwellgarceau@gmail.com>
2018-07-09 09:49:49 -0500

    Creating a web server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const app = express();
+app.use(express.static(`${__dirname}/public`));
 
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express!</h1>');
@@ -23,4 +24,6 @@ app.get('/bad', (req, res) => {
   });
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
