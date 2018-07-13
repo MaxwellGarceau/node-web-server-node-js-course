c037c5ff72d22df5a7a9462f2cc03fb457ed2bd2
Max Garceau <maxwellgarceau@gmail.com>
2018-07-13 09:05:40 -0500

    Using Express Middleware

--- server.js
+++ server.js
@@ -1,10 +1,29 @@
 const express = require('express');
 const hbs = require('hbs');
+const fs = require('fs');
 
 const app = express();
 
 hbs.registerPartials(`${__dirname}/views/partials`);
 app.set('view engine', 'hbs');
+
+app.use((req, res, next) => {
+  const now = new Date().toString();
+  const log = `${now}: ${req.method} ${req.url}`;
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();
+});
+
+// Maintenance middleware
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
 app.use(express.static(`${__dirname}/public`));
 
 hbs.registerHelper('getCurrentYear', () => {
@@ -16,14 +35,7 @@ hbs.registerHelper('screamIt', (text) => {
 });
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express!</h1>');
-  // res.send({
-  //   name: 'Max',
-  //   likes: [
-  //     'Music',
-  //     'Philosophy'
-  //   ]
-  // });
+
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website!'
