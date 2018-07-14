176c58fa2e169e915bec34203a3aaa0a15593550
Max Garceau <maxwellgarceau@gmail.com>
2018-07-14 12:58:30 -0500

    Explored testing objects and arrays

--- node-tests/utils/utils.test.js
+++ node-tests/utils/utils.test.js
@@ -1,15 +1,45 @@
+const expect = require('expect');
+
 const utils = require('./utils');
 
 it('should add two numbers', () => {
   const res = utils.add(33, 11);
-  if (res !== 44) {
-    throw new Error(`Expected 44, but got ${res}.`);
-  }
+
+  expect(res).toBe(44).toBeA('number');
+  // if (res !== 44) {
+  //   throw new Error(`Expected 44, but got ${res}.`);
+  // }
 });
 
 it('should square a number', () => {
   const res = utils.square(5);
-  if (res !== 25) {
-    throw new Error(`Expected 25, but got ${res}.`);
-  }
+
+  expect(res).toBe(25).toBeA('number');
+  // if (res !== 25) {
+  //   throw new Error(`Expected 25, but got ${res}.`);
+  // }
 });
+
+it('should verify first and last names are set', () => {
+  const user = { location: 'Nashville', age: 26 };
+  const res = utils.setName(user, 'Max Garceau');
+  
+  expect(res).toInclude({
+    firstName: 'Max',
+    lastName: 'Garceau'
+  });
+});
+
+// it('should expect some values', () => {
+//   // expect(12).toNotBe(11);
+//   // expect({name: 'Max'}).toNotEqual({name: 'max'});
+//   // expect([2,3,4,5]).toExclude(1);
+//   // expect({
+//   //   name: 'Max',
+//   //   age: 26,
+//   //   location: 'Nashville'
+//   // }).toExclude({
+//   //   age: 23
+//   // });
+
+// });
