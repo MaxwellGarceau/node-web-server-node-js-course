5e81ec62f00a7f479bd1119b78173b0e96ae9c8e
Max Garceau <maxwellgarceau@gmail.com>
2018-07-15 13:55:08 -0500

    Testing express applications part 2

--- node-tests/server/server.js
+++ node-tests/server/server.js
@@ -3,7 +3,41 @@ const express = require('express');
 const app = express();
 
 app.get('/', (request, response) => {
-  response.send('Hello world!');
+  response.send({
+    error: 'Page not found.',
+    name: 'Todo App v1.0'
+  });
 });
 
-app.listen(3000);
+app.get('/users', (request, response) => {
+  response.send([
+    {
+      name: 'Max',
+      age: 26
+    },
+    {
+      name: 'Andre',
+      age: 24
+    }
+  ]);
+});
+
+app.listen(3001);
+
+module.exports.app = app;
+
+// }, {
+
+// }, {
+//   name: 'Rafael',
+//   age: 31
+// }, {
+//   name: 'Keenan',
+//   age: 31
+// }, {
+//   name: 'Fawn',
+//   age: 30
+// }, {
+//   name: 'Kyle',
+//   age: 26
+// }
